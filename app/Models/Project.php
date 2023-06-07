aff09624092e0cfbd50b20e19b6909c614433456
GiuseppeRepici <giuseppe.repici95@gmail.com>
2023-06-07 13:58:13 +0200

    aggiunti metodi nei model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -9,4 +9,8 @@ class Project extends Model
 {
     use HasFactory;
     protected $fillable = ['name', 'slug', 'description'];
+
+    public function type() {
+        return $this->belongsTo(Type::class);
+    }
 }
